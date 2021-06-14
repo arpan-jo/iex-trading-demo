3f39dd72baa1f229783356a95162ee406c2e53c2
arpan-jo <arpanjorng@gmail.com>
2021-06-14 13:31:05 +0600

    unit changer added

--- src/components/sample/sample.js
+++ src/components/sample/sample.js
@@ -3,98 +3,118 @@ import { getData } from "../helper/helper";
 import "./sample.css";
 
 const Sample = () => {
-  const [id, setID] = useState("");
-  const [data, setData] = useState({});
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+   const [id, setID] = useState("");
+   const [data, setData] = useState({});
+   const [error, setError] = useState("");
+   const [loading, setLoading] = useState(false);
 
-  const handleOnPress = (e) => {
-    console.log(e);
-    if (e.charCode === 13) {
-      getData(id, setData, setError, setLoading);
-    }
-  };
+   const handleOnPress = (e) => {
+      console.log(e);
+      if (e.charCode === 13) {
+         getData(id, setData, setError, setLoading);
+      }
+   };
 
-  return (
-    <main class="main-section">
-      <section class="input-section">
-        <div>
-          <input
-            onKeyPress={handleOnPress}
-            type="text"
-            class="input-search"
-            placeholder="Enter your keywords"
-            onChange={(e) => setID(e.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              getData(id, setData, setError, setLoading);
-            }}
-            class="seacrh-btn"
-          >
-            SEARCH
-          </button>
-        </div>
-      </section>
-      {error && (
-        <p
-          style={{
-            color: "red",
-            textAlign: "center",
-            marginBottom: "27px",
-          }}
-        >
-          Invalid ticker symbol.
-        </p>
-      )}
+   const unitchanger = (dollar) => {
+      // Nine Zeroes for Billions
+      return Math.abs(Number(dollar)) >= 1.0e9
+         ? (Math.abs(Number(dollar)) / 1.0e9).toFixed(2) + "B"
+         : // Six Zeroes for Millions
+         Math.abs(Number(dollar)) >= 1.0e6
+         ? (Math.abs(Number(dollar)) / 1.0e6).toFixed(2) + "M"
+         : // Three Zeroes for Thousands
+         Math.abs(Number(dollar)) >= 1.0e3
+         ? (Math.abs(Number(dollar)) / 1.0e3).toFixed(2) + "K"
+         : Math.abs(Number(dollar));
+   };
 
-      {loading && <div class="loader" style={{ margin: "0 auto" }}></div>}
-      {data?.symbol && (
-        <section class="table-section">
-          <div class="table-head">
+   return (
+      <main class="main-section">
+         <section class="input-section">
             <div>
-              <p class="heading-p">{data?.symbol}</p>
-              <p class="sub-head" style={{ color: " #888888" }}>
-                {data?.companyName}
-              </p>
+               <input
+                  onKeyPress={handleOnPress}
+                  type="text"
+                  class="input-search"
+                  placeholder="Enter your keywords"
+                  onChange={(e) => setID(e.target.value)}
+               />
             </div>
             <div>
-              <p class="heading-p">{data?.latestPrice}</p>
-              <p class="sub-head" style={{ color: "#27ae60" }}>
-                ({data?.change})({data?.changePercent}%)
-                <span>&#8593;</span>
-              </p>
+               <button
+                  onClick={() => {
+                     getData(id, setData, setError, setLoading);
+                  }}
+                  class="seacrh-btn"
+               >
+                  SEARCH
+               </button>
             </div>
-          </div>
-          <hr />
-          <div>
-            <table>
-              <tr>
-                <td>Mkt Cap</td>
-                <td class="text-end">{data?.marketCap}</td>
-                <td class="text-start">Prev close</td>
-                <td class="text-end">{data?.previousClose}</td>
-              </tr>
-              <tr>
-                <td>P/E ratio</td>
-                <td class="text-end">{data?.peRatio}</td>
-                <td class="text-start">52-wk high</td>
-                <td class="text-end">{data?.week52High}</td>
-              </tr>
-              <tr>
-                <td>Avg. volume</td>
-                <td class="text-end">{data?.avgTotalVolume}</td>
-                <td class="text-start">52-wk low</td>
-                <td class="text-end">{data?.week52Low}</td>
-              </tr>
-            </table>
-          </div>
-        </section>
-      )}
-    </main>
-  );
+         </section>
+         {error && (
+            <p
+               style={{
+                  color: "red",
+                  textAlign: "center",
+                  marginBottom: "27px",
+               }}
+            >
+               Invalid ticker symbol.
+            </p>
+         )}
+
+         {loading && <div class="loader" style={{ margin: "0 auto" }}></div>}
+         {data?.symbol && (
+            <section class="table-section">
+               <div class="table-head">
+                  <div>
+                     <p class="heading-p">{data?.symbol}</p>
+                     <p class="sub-head" style={{ color: " #888888" }}>
+                        {data?.companyName}
+                     </p>
+                  </div>
+                  <div>
+                     <p class="heading-p">{data?.latestPrice}</p>
+                     <p class="sub-head" style={{ color: "#27ae60" }}>
+                        ({data?.change})({data?.changePercent}%)
+                        <span>&#8593;</span>
+                     </p>
+                  </div>
+               </div>
+               <hr />
+               <div>
+                  <table>
+                     <tr>
+                        <td>Mkt Cap</td>
+                        <td class="text-end">
+                           {
+                              // data?.marketCap
+                              unitchanger(data?.marketCap)
+                           }
+                        </td>
+                        <td class="text-start">Prev close</td>
+                        <td class="text-end">{data?.previousClose}</td>
+                     </tr>
+                     <tr>
+                        <td>P/E ratio</td>
+                        <td class="text-end">{data?.peRatio}</td>
+                        <td class="text-start">52-wk high</td>
+                        <td class="text-end">{data?.week52High}</td>
+                     </tr>
+                     <tr>
+                        <td>Avg. volume</td>
+                        <td class="text-end">
+                           {unitchanger(data?.avgTotalVolume)}
+                        </td>
+                        <td class="text-start">52-wk low</td>
+                        <td class="text-end">{data?.week52Low}</td>
+                     </tr>
+                  </table>
+               </div>
+            </section>
+         )}
+      </main>
+   );
 };
 
 export default Sample;
